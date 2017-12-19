d06d90a82c9ec0c9e047ffc40faba0784bf8ffbb
league <wjmorris2004@gmail.com>
2017-12-18 19:57:42 -0800

    myAges

---
+++ src/myAges.java
@@ -0,0 +1,19 @@
+
+public class myAges {
+public static void main(String[] args) {
+	for (int i = 2006; i <= 2017; i++ ) {
+
+	if(i-2006==1) {
+		System.out.println(" In "+i+ " I was " +(i-2006)+ " year old");
+		}
+	else if(i==2017) {
+		System.out.println(" In "+i+ " I am " +(i-2006)+ " years old");
+	}
+	else {
+		System.out.println(" In "+i+ " I was " +(i-2006)+ " years old");
+	}
+}
+}
+}
+
+
